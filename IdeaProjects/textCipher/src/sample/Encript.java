93d56ff8ac741db4144ad472857c5c24bb68e81d
Joseph Gakunga <josehwilddog@gmail.com>
2020-01-28 12:24:38 +0300

    create class constructor

--- IdeaProjects/textCipher/src/sample/Encript.java
+++ IdeaProjects/textCipher/src/sample/Encript.java
@@ -3,4 +3,9 @@ package sample;
 class Encript {
     private String inputText;
     private int key;
+
+    public Encript(String inputText, int key) {
+        this.key = key;
+        this.inputText = inputText;
+    }
 }
