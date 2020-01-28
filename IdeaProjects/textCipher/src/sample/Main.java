efd2b1e2c4d031e02d5213dd9f22f7ad0f18ee21
Joseph Gakunga <josehwilddog@gmail.com>
2020-01-28 12:30:13 +0300

    import all necessary imports

--- IdeaProjects/textCipher/src/sample/Main.java
+++ IdeaProjects/textCipher/src/sample/Main.java
@@ -1,10 +1,22 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 
-
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
         launch(args);
     }
